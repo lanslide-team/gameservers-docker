e4617cc18fd6de5dc5e230996f9d13343da11d6b
Brendon Taylor <nuke@lanslide.com.au>
2023-03-08 01:16:32 +1100

    Added custom tracklist

--- trackmania-forever/start_server.sh
+++ trackmania-forever/start_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 dedicated_cfg="/tm/GameData/Config/dedicated_cfg.txt"
+tracklist_root="/tm/GameData/Tracks"
 
 [[ -n $RCON_PASSWORD ]] || RCON_PASSWORD=$(dd if=/dev/urandom bs=20 count=1 2>/dev/null | base64)
 
@@ -8,6 +9,7 @@ sed -i "s/%%SERVER_NAME%%/$SERVER_NAME/" $dedicated_cfg
 sed -i "s/SuperAdminPassword/$RCON_PASSWORD/" $dedicated_cfg
 sed -i "s/AdminPassword/$RCON_PASSWORD/" $dedicated_cfg
 sed -i "s/UserPassword/$RCON_PASSWORD/" $dedicated_cfg
+[[ -n $TRACK ]] || TRACK="Green\B11-Race.Challenge"
 [[ -n $TRACKLIST ]] || TRACKLIST="tracklist-all.cfg"
 
 echo "Starting apache server"
@@ -15,6 +17,8 @@ service apache2 start
 
 echo "Server config dedicated_cfg.txt is"
 cat $dedicated_cfg
+
+sed -i "s/\$TRACK/${TRACK/\\/\\\\}/" $tracklist_root/tracklist-custom.cfg
 
 echo "Launching server"
 exec ./TrackmaniaServer /lan /game_settings=$TRACKLIST /dedicated_cfg=dedicated_cfg.txt /nodaemon
