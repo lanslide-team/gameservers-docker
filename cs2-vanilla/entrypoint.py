8d804d80daeef1a4aaa531245541ac9f4225d5f5
Brendon Taylor <nuke@lanslide.com.au>
2024-01-02 21:11:19 +1100

    cs2-vanilla

---
+++ cs2-vanilla/entrypoint.py
@@ -0,0 +1,142 @@
+#!/usr/bin/python3
+import os
+import subprocess
+import configparser
+import time
+
+
+base = ["./game/bin/linuxsteamrt64/cs2", "-dedicated", "-console", "-usercon", "+ip 0.0.0.0"]
+
+vars = {}
+
+# Read defaultenv config
+configpaser = configparser.ConfigParser()
+configpaser.read('defaultenv.ini')
+config = configpaser['env']
+CONFIG_DIR = '/cs2/game/csgo/cfg'
+
+# Add default envvars into vars variable
+for k, v in config.items():
+    try:
+	# Since the value from configparser will be a string, match "None" and set Nonetype
+        if v == 'None':
+            vars[str(k).upper()] = ''
+        else:
+            vars[str(k).upper()] = v
+    except KeyError:
+        continue
+
+# For each envvar stored, check to see if one was set in ENV and set it
+for k, v in vars.items():
+    try:
+        vars[k] = os.environ[k]
+    except KeyError:
+        continue
+
+if not vars.get('SERVERCFGFILE'):
+    vars['SERVERCFGFILE'] = 'server.cfg'
+
+# Set server port
+if vars.get('PORT'):
+    base.append('-port {PORT}'.format(**vars))
+
+if vars.get('SV_LAN'):
+    base.append('-sv_lan {SV_LAN}'.format(**vars))
+
+# Set Tickrate
+if vars.get('TICKRATE'):
+    base.append('-tickrate {TICKRATE}'.format(**vars))
+
+# Set GAME_TYPE
+if vars.get('GAME_TYPE'):
+    base.append('+game_type {GAME_TYPE}'.format(**vars))
+
+# Set GAME_MODE
+if vars.get('GAME_MODE'):
+    base.append('+game_mode {GAME_MODE}'.format(**vars))
+
+# Set RCON password
+if vars.get('RCON_PASSWORD'):
+    base.append('+rcon_password {RCON_PASSWORD}'.format(**vars))
+
+# Set SV password
+if vars.get('SV_PASSWORD'):
+    f = open(f'{CONFIG_DIR}/' + vars['SERVERCFGFILE'], 'a')
+    f.write('\nsv_password {SV_PASSWORD}'.format(**vars))
+    f.close()
+    base.append('+sv_password {SV_PASSWORD}'.format(**vars))
+
+# Set Max Players
+if vars.get('MAXPLAYERS_OVERRIDE'):
+#    base.append('-sv_visiblemaxplayers {MAXPLAYERS_OVERRIDE}'.format(**vars))
+    f = open(f'{CONFIG_DIR}/' + vars['SERVERCFGFILE'], 'a')
+    f.write('\nsv_visiblemaxplayers {MAXPLAYERS_OVERRIDE}'.format(**vars))
+    f.close()
+
+# Set Map Group
+if vars.get('MAP_GROUP'):
+    base.append('+map_group {MAP_GROUP}'.format(**vars))
+
+# Set Map
+if vars.get('MAP'):
+   base.append('+map {MAP}'.format(**vars))
+
+# Set Hostname
+if vars.get('SERVER_NAME'):
+    base.append('+hostname {SERVER_NAME}'.format(**vars))
+elif vars.get('HOSTNAME'):
+    base.append('+hostname {HOSTNAME}'.format(**vars))
+
+
+base.append('+servercfgfile {SERVERCFGFILE}'.format(**vars))
+
+# Set Teamnames
+if vars.get('TEAM1') and vars.get('TEAM2'):
+    f = open(f'{CONFIG_DIR}/' + vars['SERVERCFGFILE'], 'a')
+    f.write('\nmp_teamname_1 {TEAM1}\nmp_teamname_2 {TEAM2}'.format(**vars))
+    f.close()
+
+# Set Event Name
+#if vars.get('EVENT_NAME') and vars['EVENT_NAME'].strip() != '' and os.path.exists('/csgo/csgo/cfg/warmod/on_map_load.cfg'):
+#    f = open('/csgo/csgo/cfg/warmod/on_map_load.cfg', 'a')
+#    f.write('wm_competition "{EVENT_NAME}"\nwm_event "{EVENT_NAME}"\nwm_chat_prefix "{EVENT_NAME}"\n'.format(**vars))
+#    f.close()
+
+# Add STEAMTOKEN if required
+if vars.get('STEAMTOKEN'):
+    base.append('+sv_setsteamaccount {STEAMTOKEN}'.format(**vars))
+#else:
+#    f = open(f'{CONFIG_DIR}/' + vars['SERVERCFGFILE'], 'a')
+#    f.write('\nsv_lan 1\n')
+#    f.close()
+
+# Enable TV
+if vars.get('TV_ENABLE') and vars['TV_ENABLE'] == '1':
+    base.append('+tv_enable 1')
+
+# Set TV name
+if 'TV_NAME' in vars:
+    base.append('+tv_name {TV_NAME}'.format(**vars))
+    base.append('+tv_title {TV_NAME}'.format(**vars))
+
+# Set challonge match number
+#if vars.get('MATCH_NO') and vars['MATCH_NO'].strip() != '':
+#    f = open('/csgo/csgo/cfg/warmod/on_map_load.cfg', 'a')
+#    f.write('\nls_match_no "{MATCH_NO}"'.format(**vars))
+#    f.close()
+    #base.append('+ls_challonge "{LS_CHALLONGE}"'.format(**vars))
+
+if os.path.exists('csgo/scripts/mysql-files/fresh_install.sql'):
+    mysql_root_password = os.popen('echo -n l33t | sha256sum | base64 | head -c 32 ; echo').read().strip()
+#    os.system('mysql -uroot -p"{}" -hmariadb -e "CREATE DATABASE IF NOT EXISTS sourcemod;"'.format(mysql_root_password))
+#    os.system('mysql -uroot -p"{}" -hmariadb sourcemod < csgo/addons/sourcemod/configs/sql-init-scripts/mysql/create_admins.sql'.format(mysql_root_password))
+#    os.system('mysql -uroot -p"{}" -hmariadb < csgo/scripts/mysql-files/fresh_install.sql'.format(mysql_root_password))
+
+os.system("hostname -I | python3 stats.py &")
+#os.system("python3 config_admins.py > game/csgo/addons/counterstrikesharp/configs/admins.json")
+#os.makedirs("game/csgo/PugSharp/Config")
+#os.system("python3 config_match.py > game/csgo/PugSharp/Config/match.json")
+
+
+# print(base)
+subprocess.call(base)
---
+++ cs2-vanilla/stats.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+
+import asyncio
+import json
+import os
+import sys
+import time
+from rcon.source import Client
+
+
+class Query:
+    SOURCE_RESPONSE: list[str] = ['Name', 'Map', 'Players', 'MaxPlayers', 'GamePort']
+    INITIAL_SLEEP: int = 5
+    DEFAULT_SLEEP: int = 5
+
+    @classmethod
+    def __process_command(cls, client, command):
+        lines = client.run(command).split('\n')
+        output = ""
+
+        for line in lines:
+             tokens = line.split('=')
+             if len(tokens) == 2:
+                 output += tokens[1].strip()
+             else:
+                 output += line.strip()
+
+        return output
+
+    @classmethod
+    async def process_game(cls, host: str, port: int, rcon_password: str) -> dict:
+        pr = {}
+        while True:
+            try:
+                status_json = None
+                with Client(host, int(port), passwd=rcon_password) as client:
+                    hostname = cls.__process_command(client, 'hostname')
+                    mp_teamname_1 = cls.__process_command(client, 'mp_teamname_1')
+                    mp_teamname_2 = cls.__process_command(client, 'mp_teamname_2')
+                    status_json = json.loads(cls.__process_command(client, 'status_json'))
+                    sv_visiblemaxplayers = cls.__process_command(client, 'sv_visiblemaxplayers')
+                    match_state = cls.__process_command(client, 'ps_matchstate')
+                    mapscore_json = cls.__process_command(client, 'ps_mapscore_json')
+
+                    if 'match is running' in match_state.lower():
+                        match_state = None
+                    if 'match is running' in mapscore_json.lower():
+                        mapscore_json = None
+
+                    pr['Name'] = hostname
+                    pr['MaxPlayers'] = sv_visiblemaxplayers
+                    
+                    pr['MatchState'] = None if 'Unknown command' in match_state else match_state
+                    pr['MapScoreJSON'] = None if 'Unknown command' in mapscore_json else mapscore_json
+
+                    try:
+                        pr['Players'] = status_json['server']['clients_human']
+                    except KeyError:
+                        pr['Players'] = None
+
+                    try:
+                        pr['Map'] = status_json['server']['map']
+                    except KeyError:
+                        pr['Map'] = None
+
+            except asyncio.exceptions.TimeoutError:
+                pr = {'Error': 'Timeout'}
+            except: 
+                pr = {'Error': 'Timeout'}
+
+            with open('/cs2/stats.json', 'w', encoding='utf-8') as f:
+                json.dump(pr, f, ensure_ascii=False, indent=4)
+
+            time.sleep(cls.DEFAULT_SLEEP)
+
+
+if __name__ == '__main__':
+    host = input().strip()
+    time.sleep(Query.INITIAL_SLEEP)
+    asyncio.run(Query.process_game(host=host, port=os.environ['PORT'], rcon_password=os.environ['RCON_PASSWORD']))
+
