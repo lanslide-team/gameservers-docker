e24e4fa5746b681d3fe6d85c0bc244bc139d6f6b
Brendon Taylor <nuke@lanslide.com.au>
2024-03-14 16:23:52 +1100

    Updated entry point

--- cs2-wingman/entrypoint.py
+++ cs2-wingman/entrypoint.py
@@ -4,6 +4,22 @@ import subprocess
 import configparser
 import time
 
+def already_in_config(file: str, search_txt: str) -> bool:
+    with open(file) as f:
+        if search_txt in f.read():
+            return True
+    return False
+
+def find_or_replace(file: str, search_txt: str, replace_txt: str) -> None:
+    if already_in_config(file, search_txt):
+        os.system(f"sed -i '/{search_txt}/c\{replace_txt}' {file}")
+    else:
+        os.system(f"echo '\n{replace_txt}' >> {file}")
+
+#    os.system(f"sed -i '/matchzy_knife_enabled_default/c\matchzy_knife_enabled_default {'true' if vars['ENABLE_KNIFE'] == '1' else 'false'}' {matchzy_cfg}")
+# echo 'matchzy_remote_log_url \"https://portal.lanslide.com.au/api/v1/matches/{vars['MATCH_ID']}\"' >> {warmup_cfg}
+
+
 
 base = ["./game/bin/linuxsteamrt64/cs2", "-dedicated", "-console", "-usercon", "+ip 0.0.0.0"]
 
@@ -135,9 +151,7 @@ if os.path.exists('csgo/scripts/mysql-files/fresh_install.sql'):
 os.system("hostname -I | python3 stats.py &")
 os.system("python3 config_admins.py > game/csgo/cfg/MatchZy/admins.json")
 os.system("python3 config_admins.py > game/csgo/addons/counterstrikesharp/configs/admins.json")
-#os.makedirs("game/csgo/PugSharp/Config")
 os.system("python3 config_match.py > game/csgo/match.json")
-#os.system("python3 config_server.py > game/csgo/PugSharp/Config/server.json")
 
 config_root = 'game/csgo/cfg/MatchZy'
 matchzy_cfg = config_root + '/config.cfg'
@@ -145,28 +159,49 @@ live_cfg = config_root + '/live.cfg'
 warmup_cfg = config_root + '/warmup.cfg'
 
 # Modify MatchZy Settings
-os.system(f"sed -i '/matchzy_knife_enabled_default/c\matchzy_knife_enabled_default {'true' if vars['ENABLE_KNIFE'] == '1' else 'false'}' {matchzy_cfg}")
-os.system(f"sed -i '/matchzy_minimum_ready_required/c\matchzy_minimum_ready_required {vars['MIN_PLAYERS_TO_READY']}' {matchzy_cfg}")
-os.system(f"sed -i '/matchzy_chat_prefix/c\matchzy_chat_prefix [{{Green}}{vars['EVENT_NAME']}{{Default}}]' {matchzy_cfg}")
-os.system(f"sed -i '/matchzy_playout_enabled_default/c\matchzy_playout_enabled_default {'true' if vars['PLAYOUT_ENABLED'] == '1' else 'false'}' {matchzy_cfg}")
-os.system(f"sed -i '/matchzy_demo_upload_url/c\matchzy_demo_upload_url \"{vars['DEMO_UPLOAD_URL']}\"' {matchzy_cfg}")
-os.system(f"sed -i '/matchzy_autostart_mode/c\matchzy_autostart_mode {vars['AUTOSTART_MODE']}' {matchzy_cfg}")
-os.system(f"sed -i '/matchzy_use_pause_command_for_tactical_pause/c\matchzy_use_pause_command_for_tactical_pause {'true' if vars['USE_PAUSE_FOR_TECH'] == '1' else 'false'}' {matchzy_cfg}")
+#os.system(f"sed -i '/matchzy_knife_enabled_default/c\matchzy_knife_enabled_default {'true' if vars['ENABLE_KNIFE'] == '1' else 'false'}' {matchzy_cfg}")
+#os.system(f"sed -i '/matchzy_minimum_ready_required/c\matchzy_minimum_ready_required {vars['MIN_PLAYERS_TO_READY']}' {matchzy_cfg}")
+#os.system(f"sed -i '/matchzy_chat_prefix/c\matchzy_chat_prefix [{{Green}}{vars['EVENT_NAME']}{{Default}}]' {matchzy_cfg}")
+#os.system(f"sed -i '/matchzy_playout_enabled_default/c\matchzy_playout_enabled_default {'true' if vars['PLAYOUT_ENABLED'] == '1' else 'false'}' {matchzy_cfg}")
+#os.system(f"sed -i '/matchzy_demo_upload_url/c\matchzy_demo_upload_url \"{vars['DEMO_UPLOAD_URL']}\"' {matchzy_cfg}")
+#os.system(f"sed -i '/matchzy_autostart_mode/c\matchzy_autostart_mode {vars['AUTOSTART_MODE']}' {matchzy_cfg}")
+#os.system(f"sed -i '/matchzy_use_pause_command_for_tactical_pause/c\matchzy_use_pause_command_for_tactical_pause {'true' if vars['USE_PAUSE_FOR_TECH'] == '1' else 'false'}' {matchzy_cfg}")
+
+# find_or_replace(file, search_txt, replace_txt)
+find_or_replace(matchzy_cfg, 'matchzy_knife_enabled_default', f"matchzy_knife_enabled_default {'true' if vars['ENABLE_KNIFE'] == '1' else 'false'}")
+find_or_replace(matchzy_cfg, 'matchzy_minimum_ready_required', f"matchzy_minimum_ready_required {vars['MIN_PLAYERS_TO_READY']}")
+find_or_replace(matchzy_cfg, 'matchzy_chat_prefix', f"matchzy_chat_prefix [{{Green}}{vars['EVENT_NAME']}{{Default}}]")
+find_or_replace(matchzy_cfg, 'matchzy_playout_enabled_default', f"matchzy_playout_enabled_default {'true' if vars['PLAYOUT_ENABLED'] == '1' else 'false'}")
+find_or_replace(matchzy_cfg, 'matchzy_demo_upload_url', f"matchzy_demo_upload_url \"{vars['DEMO_UPLOAD_URL']}\"")
+find_or_replace(matchzy_cfg, 'matchzy_autostart_mode', f"matchzy_autostart_mode {vars['AUTOSTART_MODE']}")
+find_or_replace(matchzy_cfg, 'matchzy_use_pause_command_for_tactical_pause', f"matchzy_use_pause_command_for_tactical_pause {'true' if vars['USE_PAUSE_FOR_TECH'] == '1' else 'false'}")
 
 # Add new command
-os.system(f"echo '\nmatchzy_enable_match_scrim true' >> {matchzy_cfg}")
+#os.system(f"echo '\nmatchzy_enable_match_scrim true' >> {matchzy_cfg}")
+find_or_replace(matchzy_cfg, 'matchzy_enable_match_scrim', f"matchzy_enable_match_scrim true")
 
 # Add exta commands to the end
-os.system(f"echo 'matchzy_remote_log_url \"https://portal.lanslide.com.au/api/v1/matches/{vars['MATCH_ID']}\"' >> {warmup_cfg}")
-os.system(f"echo 'matchzy_remote_log_header_key Authorization' >> {warmup_cfg}")
-os.system(f"echo 'matchzy_remote_log_header_value \"6583bac9a2455\"' >> {warmup_cfg}")
+#os.system(f"echo 'matchzy_remote_log_url \"https://portal.lanslide.com.au/api/v1/matches/{vars['MATCH_ID']}\"' >> {warmup_cfg}")
+#os.system(f"echo 'matchzy_remote_log_header_key Authorization' >> {warmup_cfg}")
+#os.system(f"echo 'matchzy_remote_log_header_value \"6583bac9a2455\"' >> {warmup_cfg}")
+
+find_or_replace(warmup_cfg, 'matchzy_remote_log_url', f"matchzy_remote_log_url \"https://portal.lanslide.com.au/api/v1/matches/{vars['MATCH_ID']}\"")
+find_or_replace(warmup_cfg, 'matchzy_remote_log_header_key', f"matchzy_remote_log_header_key Authorization")
+find_or_replace(warmup_cfg, 'matchzy_remote_log_header_value', f"matchzy_remote_log_header_value \"6583bac9a2455\"")
 
 # Update live config
-os.system(f"sed -i '/mp_team_timeout_max/c\mp_team_timeout_max 3' {live_cfg}")
-os.system(f"sed -i '/mp_freezetime/c\mp_freezetime {vars['FREEZETIME']}' {live_cfg}")
-os.system(f"sed -i '/mp_overtime_startmoney/c\mp_overtime_startmoney {vars['OVERTIME_STARTMONEY']}' {live_cfg}")
+#os.system(f"sed -i '/mp_team_timeout_max/c\mp_team_timeout_max 3' {live_cfg}")
+#os.system(f"sed -i '/mp_freezetime/c\mp_freezetime {vars['FREEZETIME']}' {live_cfg}")
+#os.system(f"sed -i '/mp_overtime_startmoney/c\mp_overtime_startmoney {vars['OVERTIME_STARTMONEY']}' {live_cfg}")
 
-os.system(f"echo '\nmp_technical_timeout_duration_s 300' >> {live_cfg}")
+find_or_replace(live_cfg, 'mp_team_timeout_max', f"mp_team_timeout_max 3")
+find_or_replace(live_cfg, 'mp_freezetime', f"mp_freezetime {vars['FREEZETIME']}")
+find_or_replace(live_cfg, 'mp_overtime_startmoney', f"mp_overtime_startmoney {vars['OVERTIME_STARTMONEY']}")
+
+#os.system(f"echo '\nmp_technical_timeout_duration_s 300' >> {live_cfg}")
+find_or_replace(live_cfg, 'nmp_technical_timeout_duration_s', f"mp_technical_timeout_duration_s 300")
+
+
 
 # print(base)
 subprocess.call(base)
