d726eae666411e96b4e8238756a13a752a1043e1
Brendon Taylor <nuke@lanslide.com.au>
2024-08-05 18:14:30 +1000

    MC update

--- build-mc.sh
+++ build-mc.sh
@@ -3,10 +3,10 @@ set -e
 
 STARTT=$(date +%s.%N)
 
-docker build -t='base:latest' base/
-docker build -t='minecraft:latest' minecraft/ --no-cache
+#docker build -t='base:latest' base/
+#docker build -t='minecraft:latest' minecraft/ --no-cache
 docker build -t='minecraft-base:latest' minecraft-base/ --no-cache
-docker build -t='minecraft-build:latest' minecraft-build/ --no-cache
+#docker build -t='minecraft-build:latest' minecraft-build/ --no-cache
 docker build -t='minecraft-map:latest' minecraft-map/ --no-cache
 
 docker image prune -f
