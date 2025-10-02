342a551789f3db0e4e74ab6f4155f837f0ba65af
LAN-slide Team <nuke@lanslide.com.au>
2025-10-02 16:08:27 +1000

    update

--- build-mc.sh
+++ build-mc.sh
@@ -6,7 +6,7 @@ STARTT=$(date +%s.%N)
 #docker build -t='base:latest' base/
 #docker build -t='minecraft:latest' minecraft/ --no-cache
 docker build -t='minecraft-base:latest' minecraft-base/ --no-cache
-#docker build -t='minecraft-build:latest' minecraft-build/ --no-cache
+docker build -t='minecraft-build:latest' minecraft-build/ --no-cache
 docker build -t='minecraft-map:latest' minecraft-map/ --no-cache
 
 docker image prune -f
