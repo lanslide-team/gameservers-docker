2e82aa951f55747a70629399bbe9df5d421dbf91
LAN-slide Team <nuke@lanslide.com.au>
2026-02-13 18:13:32 +1100

    update

--- trackmania-forever/start_server.sh
+++ trackmania-forever/start_server.sh
@@ -12,6 +12,16 @@ sed -i "s/UserPassword/$RCON_PASSWORD/" $dedicated_cfg
 [[ -n $TRACK ]] || TRACK="Green\B11-Race.Challenge"
 [[ -n $TRACKLIST ]] || TRACKLIST="tracklist-all.cfg"
 
+RCON_MD5="$(printf '%s' "$RCON_PASSWORD" | md5sum | awk '{print $1}')"
+sed -i "s|^[[:space:]]*const[[:space:]]\+PASSWORD[[:space:]]*=[[:space:]]*'.*';|      const PASSWORD = '${RCON_MD5}';|" \
+  /var/www/html/config/adminserv.cfg.php
+
+echo "ServerName localhost" > /etc/apache2/conf-available/servername.conf
+a2enconf servername >/dev/null 2>&1 || true
+
+echo "Starting apache"
+apachectl -D FOREGROUND &
+
 echo "Starting apache server"
 service apache2 start
 
@@ -19,6 +29,7 @@ echo "Server config dedicated_cfg.txt is"
 cat $dedicated_cfg
 
 sed -i "s/\$TRACK/${TRACK/\\/\\\\}/" $tracklist_root/tracklist-custom.cfg
+
 
 echo "Launching server"
 exec ./TrackmaniaServer /lan /game_settings=$TRACKLIST /dedicated_cfg=dedicated_cfg.txt /nodaemon
